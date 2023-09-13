9d1189bc41ff723c73661980dcfbf7cf50761294
Forxai <136213577+Forxai@users.noreply.github.com>
2023-09-13 09:17:58 -0500

    Add files via upload

---
+++ Conditional/ConditionalExample2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Conditionalexamples;
+
+public class ConditionalExample2 {
+    
+    public static void main(String[] args) {
+    
+        int x = 10;  
+        int y = 12;  
+    
+        if(x+y < 10) {  
+            System.out.println("x + y is less than      10");  
+        }   
+    else {  
+        System.out.println("x + y is greater than 20");  
+        }  
+    }
+}    
---
+++ Conditional/ConditionalExamples.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package conditionalexamples;
+
+public class ConditionalExamples {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 10;    
+        int y = 12;    
+        if(x+y > 20) {    
+            System.out.println("x + y is greater than 20");    
+        }    
+    }      
+}
+    
