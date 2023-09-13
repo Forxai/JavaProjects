167de8254d0615f0a2f8f87b6d548506203eb6ee
Forxai <136213577+Forxai@users.noreply.github.com>
2023-09-13 09:06:40 -0500

    Add files via upload

---
+++ Introduccion/Droid.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package droid;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Droid {
+
+   int batteryLevel;
+  String name;
+
+  public Droid(String droidName) {
+    batteryLevel = 100;
+    name = droidName;
+  }
+
+  public String toString() {
+    return "Hello, I'm the droid " + name;
+  }
+
+  public void performTask(String task) {
+    System.out.println(name + " is performing " + task); 
+  }
+  public void batteryLevelLosing() {
+    batteryLevel -= 10; 
+  }
+
+  public void energyReport() {
+    System.out.println(batteryLevel);
+  }
+
+  public void energyTransfer() {
+    System.out.println(batteryLevel);
+  }
+
+  public static void main(String[] args) {
+    Droid Codey = new Droid("Codey");
+    //System.out.println(Codey.name);       
+    System.out.println(Codey.toString());
+    Codey.performTask("dancing");
+    Codey.batteryLevelLosing();
+    Codey.energyReport();
+
+
+  }
+    
+}
