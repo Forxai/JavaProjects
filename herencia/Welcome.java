e24f954611198e5003c458139bf99ed8ba5a9f03
Forxai <136213577+Forxai@users.noreply.github.com>
2023-10-05 09:23:39 -0500

    Add files via upload

---
+++ herencia/Welcome.java
@@ -0,0 +1,14 @@
+
+package welcome;
+import javax.swing.JOptionPane;
+
+public class Welcome {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JOptionPane.showMessageDialog(null,"!Wlcome\nto\nthe java world¡");
+        System.exit(0);
+    }
+    
+}
